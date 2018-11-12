76332fe7c27c0e34c0c8af3a0a78cdb9c38db7b0
yangjian <18263800587@163.com>
2018-11-12 21:56:27 +0800

    第一次提交

---
+++ CreateView.h
@@ -0,0 +1,23 @@
+//
+//  CreateView.h
+//  GetMoney
+//
+//  Created by yangjian on 2018/11/12.
+//  Copyright © 2018 zhangshuyue. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+@interface CreateView : NSObject
+
++(UIButton *)createButtonWithFrame:(CGRect)frame title:(NSString *)title titleColor:(UIColor *)titleColor font:(CGFloat)font backgroundColor:(UIColor *)bgColor cornerRadius:(CGFloat)radius;
+
+
+
+@end
+
+//NS_ASSUME_NONNULL_BEGIN
+
+//NS_ASSUME_NONNULL_END
