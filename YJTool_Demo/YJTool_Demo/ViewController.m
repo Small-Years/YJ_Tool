0ea3c965a6e9eabd84e6957983369ca1485f081a
yangjian <18263800587@163.com>
2018-11-27 15:37:51 +0800

    更改Pod文件夹

--- YJTool_Demo/YJTool_Demo/ViewController.m
+++ YJTool_Demo/YJTool_Demo/ViewController.m
@@ -5,8 +5,11 @@
 //  Created by yangjian on 2018/11/13.
 //  Copyright © 2018 zhangshuyue. All rights reserved.
 //
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
 
 #import "ViewController.h"
+#import "ShowWarnView.h"
 
 @interface ViewController ()
 
@@ -14,9 +17,28 @@
 
 @implementation ViewController
 
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    
+    UIView *mainView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH-60, 200)];
+    mainView.backgroundColor = [UIColor whiteColor];
+    mainView.layer.cornerRadius = 5;
+    
+    ShowWarnView *vc = [[ShowWarnView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+    vc.centerView = mainView;
+    [vc show];
+    
+    UIButton *btn = [UIButton buttonWithFrame:CGRectMake(0, 100, 100, 100) backgroundColor:[UIColor redColor] image:nil title:nil titleColor:nil titleFont:16 action:@selector(btnClicked) target:self];
+    [self.view addSubview:btn];
+}
+
+-(void)btnClicked{
+    NSLog(@"btn");
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 
--- YJTool_Demo/YJTool_Demo/YJ_Tool/ToolPchHeader.h
+++ YJTool_Demo/YJTool_Demo/YJ_Tool/ToolPchHeader.h
@@ -12,7 +12,17 @@
 
 #endif /* pchHeader_h */
 
+#import "KeyDefine.h"
 
+#import "UIView+Extension.h"
+#import "UITableView+EmptyData.h"
+
+//创建view
+#import "UIButton+createBtn.h"
+#import "UILabel+createLabel.h"
+#import "UIImageView+createImgView.h"
+
+//image处理
 #import "UIImage+Color.h"
 #import "UIImage+Rotate.h"
 #import "UIImage+Gif.h"
