2def85165b38020bd3dc94cceac56c4949d829f1
yangjian <18263800587@163.com>
2020-06-23 11:21:47 +0800

    修改提示框

--- YJTool_Demo/ViewController.m
+++ YJTool_Demo/ViewController.m
@@ -9,7 +9,7 @@
 #define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
 
 #import "ViewController.h"
-#import "ShowWarnView.h"
+#import "YJShowAlertView.h"
 
 @interface ViewController ()
 
@@ -27,7 +27,8 @@
     [self.view addSubview:mainView];
     [YJAllmethod addShadowToView:mainView color:RGB(76, 76, 76) shadowWidth:5 shadowType:UIShadowSideTypeRight];
     [YJAllmethod addShadowToView:mainView color:RGB(76, 76, 76) shadowWidth:5 shadowType:UIShadowSideTypeBottom];
-//    ShowWarnView *vc = [[ShowWarnView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+    
+//    YJShowAlertView *vc = [[YJShowAlertView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
 //    vc.centerView = mainView;
 //    [vc show];
 //
@@ -47,5 +48,23 @@
     
 }
 
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+    UILabel *alertLable = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 150, 60)];
+    alertLable.backgroundColor = [UIColor blackColor];
+    alertLable.layer.cornerRadius = 5;
+    alertLable.text = @"保存成功";
+    alertLable.layer.masksToBounds = YES;
+    alertLable.textAlignment = NSTextAlignmentCenter;
+    alertLable.textColor = [UIColor whiteColor];
+    alertLable.font = [UIFont systemFontOfSize:15];
+    
+    [YJShowAlertView showView:alertLable withType:YJShowAlertViewStyleCustom];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [YJShowAlertView dismiss];
+    });
+}
 
 @end
